9000a0581e67099267e792032a4f3489a01c660b
Tobias <113835194+optobimus@users.noreply.github.com>
2023-05-05 18:47:45 +0200

    Set up Spring Boot backend

---
+++ backend/src/main/java/com/springreactnotes/notesapp/NoteController.java
@@ -0,0 +1,13 @@
+package com.springreactnotes.notesapp;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/notes")
+public class NoteController {
+    @Autowired
+    private NoteService noteService;
+}
