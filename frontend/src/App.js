0981ce469dfc4d25543c053833c43b87b5aed154
Tobias <113835194+optobimus@users.noreply.github.com>
2023-05-08 19:09:33 +0200

    Remove API call & set date through backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,63 +1,61 @@
-import { useState } from "react";
-import { nanoid } from "nanoid";
+import { useEffect, useState } from "react";
 import NotesList from "./components/NotesList";
 import Search from "./components/Search";
 
 
 const App = () => {
-  const [notes, setNotes] = useState([
-    {
-      id: nanoid(),
-      title: "Erste Notiz",
-      text: "Das ist meine erste Notiz",
-      date: "05/02/2020",
-    },
-    {
-      id: nanoid(),
-      title: "Einkaufliste",
-      text: "Bananen, Speck, Eier",
-      date: "06/05/2020",
-    },
-    {
-      id: nanoid(),
-      title: "blabla",
-      text: "lorem ipsum ....",
-      date: "20/01/2022",
-    },
-    {
-      id: nanoid(),
-      title: "Titel hier schreiben",
-      text: "Das ist der Text meiner vierten Notiz",
-      date: "05/05/2023",
-    },
 
-  ]);
   const [searchText, setSearchText] = useState("");
 
-  const addNote = (title, text) => {
-    const date = new Date();
-    const newNote = {
-      id: nanoid(),
-      title: title,
-      text: text,
-      date: date.toLocaleDateString(),
-    }
-    const newNotes = [...notes, newNote];
-    setNotes(newNotes);
-  }
+  const [notes, setNotes] = useState([]);
 
-  const deleteNode = (id) => {
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const fetchNotes = async () => {
+    const response = await fetch("http://localhost:8080/api/notes");
+    const data = await response.json();
+    console.log(data);
+    setNotes(data);
+  };
+
+const addNote = async (title, text) => {
+  try {
+    const newNote = {
+        title: title,
+        text: text,
+    };
+    const response = await fetch("http://localhost:8080/api/notes", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newNote),
+    });
+    const data = await response.json();
+    setNotes([...notes, data]);
+  } catch (error) {
+    console.error("Error while adding note:", error);
+  }
+    
+};
+
+  const deleteNode = async (id) => {
+    await fetch(`http://localhost:8080/api/notes/${id}`, {
+      method: "DELETE",
+    });
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
-  }
-
+  };
 
   return(
     <div className="notes-container">
       <h1>Notizen</h1>
       <Search handleSearch={setSearchText}/>
       <NotesList 
-        notes={notes.filter((note) => note.text.toLowerCase().includes(searchText) || note.title.toLowerCase().includes(searchText))} 
+        notes={Array.isArray(notes) ? notes.filter((note) => note.text?.toLowerCase().includes(searchText) || note.title?.toLowerCase().includes(searchText)) : []} 
         handleAddNote={addNote} 
         handleDeleteNode={deleteNode}/>
     </div>
