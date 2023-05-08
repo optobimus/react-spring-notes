f327f1d2e1165f914dd3eeee1a23ea00160be8cc
Tobias <113835194+optobimus@users.noreply.github.com>
2023-05-08 19:10:02 +0200

    Set up NoteController class for API call routing

--- backend/src/main/java/com/springreactnotes/notesapp/NoteController.java
+++ backend/src/main/java/com/springreactnotes/notesapp/NoteController.java
@@ -1,4 +1,33 @@
 package com.springreactnotes.notesapp;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notes")
+@CrossOrigin(origins = "http://localhost:3000")     // Erlaube der React-App Zugriffe auf das Backend machen zu können
 public class NoteController {
+
+    private final NoteService noteService;
+
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @GetMapping
+    public List<Note> getAllNotes() {
+        return noteService.getAllNotes();
+    }
+
+    @PostMapping
+    public Note addNote(@RequestBody Note note) {
+        System.out.println(note);
+        return noteService.addNote(note);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteNote(@PathVariable String id) {
+        noteService.deleteNote(id);
+    }
 }
